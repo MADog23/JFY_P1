97353178258af3ad095b8f9b57b6797d84c39f3b
MADog23 <matt.dezemplen@gmail.com>
2026-09-01 13:33:21 -0500

    Remove stray duplicate jfy-alterations folder again, fix .gitignore

--- jfy-alterations/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from "next/navigation";
-import { getOptionalSession } from "@/lib/auth";
-
-export default async function HomePage() {
-  const session = await getOptionalSession();
-  if (!session) redirect("/login");
-  redirect(session.role === "MANAGER" ? "/manager" : "/employee");
-}
